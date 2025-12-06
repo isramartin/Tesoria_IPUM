8b50247b7c09839f74dd5d6cae4d502dd5ca1471
isra <isramartin157@gmail.com>
2025-12-05 20:34:08 -0600

    fix menu

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -8,8 +8,29 @@ import { Component } from '@angular/core';
 })
 export class Sidebar {
   menuAbierto = false;
+  userMenuAbierto = false;
 
   toggleMenu() {
     this.menuAbierto = !this.menuAbierto;
   }
+
+  toggleUserMenu() {
+    this.userMenuAbierto = !this.userMenuAbierto;
+  }
+
+  verPerfil() {
+    console.log('Ir al perfil...');
+  }
+
+  cerrarSesion() {
+    console.log('Cerrar sesión...');
+    // Aquí luego integras tu logout real
+  }
+
+  // Perfil de usuario (simulado por ahora)
+  usuario = {
+    nombre: 'Invitado',
+    rol: 'Miembro',
+    foto: null,
+  };
 }
