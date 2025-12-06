e672fe5af8d75bc69112686384c06eeb13e5e50c
isra <isramartin157@gmail.com>
2025-12-05 22:49:39 -0600

    fix menu

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -11,7 +11,25 @@ import { RouterOutlet } from '@angular/router';
 export class Layout {
   sidebarClosed = false;
 
+  sidebarMini = false;
+
   toggleSidebar() {
-    this.sidebarClosed = !this.sidebarClosed;
+    // Si está abierto normal → pasa a mini
+    if (!this.sidebarMini && !this.sidebarClosed) {
+      this.sidebarMini = true;
+      this.sidebarClosed = false;
+    }
+    // Si está en mini → se abre completo
+    else if (this.sidebarMini) {
+      this.sidebarMini = false;
+      this.sidebarClosed = false;
+    }
+    // Si por alguna razón está cerrado del todo → se abre normal
+    else {
+      this.sidebarMini = false;
+      this.sidebarClosed = false;
+    }
+
+    console.log('sidebarMini:', this.sidebarMini);
   }
 }
