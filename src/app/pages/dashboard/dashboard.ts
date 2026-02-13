28774755a62f1d32056ffdf5f7fb92f063dc0621
isra <isramartin157@gmail.com>
2026-02-12 20:46:24 -0600

    fix dashboard barra

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+  ngAfterViewInit() {
+    const ctx = document.getElementById('incomeChart') as HTMLCanvasElement;
 
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Talentos', 'Ofrendas', 'Externos'],
+        datasets: [
+          {
+            data: [5000, 5600, 15800],
+            backgroundColor: '#000',
+            borderRadius: 8,
+          },
+        ],
+      },
+      options: {
+        plugins: { legend: { display: false } },
+        scales: {
+          y: { beginAtZero: true },
+        },
+      },
+    });
+  }
 }
