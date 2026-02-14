e0ed3d0d77527e513daa4d820567be88959adcb0
isra <isramartin157@gmail.com>
2026-02-14 15:00:51 -0600

    fix meenu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Layout } from './layout/layout';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { ReportesInformes } from './pages/reportes-informes/reportes-informes';
 
 export const routes: Routes = [
 
@@ -10,6 +11,7 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: Dashboard },
+      { path: 'reportes', component: ReportesInformes },
       // después agregamos los demás módulos
     ]
   }
